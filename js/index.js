ed0fc8dc574f43e2fb9c9268568fe0e84c56f295
kalinin.ilya <kalinin.ilya@ergocentr.ru>
2024-05-22 16:48:53 +0300

    fix8

--- js/index.js
+++ js/index.js
@@ -81,4 +81,47 @@ document.addEventListener('DOMContentLoaded', function() {
 
     checkAllFieldsFilled(); // Проверяем, заполнены ли все поля при загрузке страницы
 });
+//////////////////////////Последний элемент точка в span 
+document.addEventListener('DOMContentLoaded', function() {
+    const inputs = document.querySelectorAll('.placeholder');
+    const requiredFieldsMsg = document.getElementById('required-fields-msg');
+    const labels = document.querySelectorAll('.text_in_ankets label');
 
+    function checkAllFieldsFilled() {
+        const allFieldsFilled = Array.from(inputs).every(input => input.value.trim() !== '');
+        requiredFieldsMsg.style.display = allFieldsFilled ? 'none' : 'block';
+    }
+
+    function updatePunctuation() {
+        let visibleLabels = Array.from(labels).filter(label => label.style.display !== 'none');
+        
+        visibleLabels.forEach((label, index) => {
+            label.textContent = label.textContent.replace(/[,.]*$/, '');
+            if (index < visibleLabels.length - 1) {
+                label.textContent += ',';
+            } else {
+                label.textContent += '.';
+            }
+        });
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener('input', function() {
+            const spanId = input.id + "-required";
+            const span = document.getElementById(spanId);
+
+            if (input.value.trim() !== '') {
+                span.style.display = 'none';
+            } else {
+                span.style.display = 'inline';
+            }
+
+            updatePunctuation();
+        });
+
+        input.addEventListener('input', checkAllFieldsFilled);
+    });
+
+    checkAllFieldsFilled(); // Проверяем, заполнены ли все поля при загрузке страницы
+    updatePunctuation(); // Устанавливаем правильные знаки препинания при загрузке страницы
+});
